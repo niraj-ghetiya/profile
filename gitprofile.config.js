191a7ce5dc7d530cff65292653883907828e8f44
Niraj Ghetiya <104854542+niraj003@users.noreply.github.com>
2023-03-31 15:37:54 +0530

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -24,7 +24,7 @@ const config = {
     skype: '',
     telegram: '',
     website: 'https://niraj-ghetiya.netlify.app/',
-    phone: '9978003439',
+    phone: '9978003438',
     email: 'nirajghetiya2002@gmail.com',
   },
   resume: {
